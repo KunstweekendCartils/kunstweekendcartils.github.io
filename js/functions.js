adf4b44757c57a2bbb6a25ca6cbd7bf0c830fc81
Laurenshup <laurens.hupperichs@gmail.com>
2023-05-06 16:49:14 +0200

    Changes to some participants.

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,9 @@
 function viewParticipant(number, object) {
     const id = ["fransVanStraaten", "babkeMoelee", "miaAmerica", "janVerschueren",
-        "suusElkink", "joseeBastianen", "mariejoseVanDerMeer", "helgaVanDerPoel", "johannesDonner",
+        "suusElkink", "joseeBastianen", "mariejoseVanDerMeer", "helgaVanDerPoel",
         "klaudiaMagyar", "ingridCapozzi", "tanjaDeRijkVanDerSteen", "miraPytze", "willSchropp",
         "guiseppeLamers", "gwendolijnKoppert", "johnPenning", "joopVanSchie", "josSolberg",
-        "leiHannen", "leoGerritsen", "nicHeffels", "sjraBosboom", "theresieTholen"][number - 1];
+        "leiHannen", "leoGerritsen", "nicHeffels", "sjraBosboom", "theresieTholen", "thuurBemelmans"][number - 1];
     gtag('event', 'open_participant', {
         'participant_id': number,
         'participant_name': id
